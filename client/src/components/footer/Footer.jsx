4ef58e3ecd25b36af96a227978e364b288be32b2
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-01-16 18:56:46 +0300

    feat: add footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.scss';
+
+export default function Footer() {
+    return (
+        <footer className='footer-wrapper'>
+            <div className='footer-text'>Vitali Zhybuliou since 2022</div>
+        </footer>
+    )
+}
