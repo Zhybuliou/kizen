a9a598e068b3966bf552c55d23d6d970de6902eb
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-01-19 01:02:32 +0300

    feat: add login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,24 @@ import Footer from './components/footer/Footer';
 import Registration from './components/registration/Registration';
 import Singup from './components/singup/Singup';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 
 function App() {
+  const isAuth = useSelector(state => state.user.isAuth);
+
+
   return (
     <BrowserRouter>
     <div>
      <Navbar/>
+     {!isAuth &&
       <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/registration" element={<Registration />} />
       <Route exact path="/sing-up" element={<Singup />} />
       </Routes>
+    }
      <Footer />
     </div>
     </BrowserRouter>
--- client/src/components/singup/Singup.jsx
+++ client/src/components/singup/Singup.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './singup.scss';
 import Input from '../input/Input';
 import Button from '../button/Button';
+import { singUp } from '../../actions/user';
+import { useState } from 'react';
+import {useDispatch} from 'react-redux';
 
 export default function Singup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
     return (
         <div className='sing-up'>
             <h2>Sing Up</h2>
-            <Input type="text" placeholder="Name"/>
-            <Input type="password" placeholder="Password"/>
+            <Input value={email} setValue={setEmail} type="email" placeholder="email"/>
+            <Input  value={password} setValue={setPassword} type="password" placeholder="Password"/>
             <Button
                   border="none"
                   color="#ccc"
@@ -16,6 +22,7 @@ export default function Singup() {
                   radius = "25px"
                   width = "241px"
                   children = "Enter"
+                  onClick={() => dispatch(singUp(email, password))}
                 ></Button>
         </div>
     )
